0323270ca68938f0230722d8ff370f680f2d848f
wzt1229 <wzt1229@126.com>
2019-06-16 22:39:05 +0800

    注释更新

--- TouchPainter_Wzt/TouchPainter_Wzt/View Controllers/CanvasViewController/CanvasViewController.m
+++ TouchPainter_Wzt/TouchPainter_Wzt/View Controllers/CanvasViewController/CanvasViewController.m
@@ -23,13 +23,10 @@
 {
   if (scribble_ != aScribble)
   {
-      scribble_ = aScribble; //tyler
-    //[scribble_ autorelease];
-    //scribble_ = [aScribble retain];
+      scribble_ = aScribble;
     
-    // add itself to the scribble as
-    // an observer for any changes to
-    // its internal state - mark
+    // add itself to the scribble as an observer for any changes to
+    // its internal state
     [scribble_ addObserver:self
                 forKeyPath:@"mark"
                    options:NSKeyValueObservingOptionInitial | 
@@ -39,82 +36,51 @@
 }
 
 
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad 
 {
-  [super viewDidLoad];
+    [super viewDidLoad];
   
-  // Get a default canvas view
-  // with the factory method of 
-  // the CanvasViewGenerator
-    CanvasViewGenerator *defaultGenerator = [[CanvasViewGenerator alloc] init];// autorelease];//tyler
-  [self loadCanvasViewWithGenerator:defaultGenerator];
+    // Get a default canvas view with the factory method of the CanvasViewGenerator
+    CanvasViewGenerator *defaultGenerator = [[CanvasViewGenerator alloc] init];
+    [self loadCanvasViewWithGenerator:defaultGenerator];
   
-  // initialize a Scribble model
-    Scribble *scribble = [[Scribble alloc] init];// autorelease]; //tyler
-  [self setScribble:scribble];
+    // initialize a Scribble model
+    Scribble *scribble = [[Scribble alloc] init];
+    [self setScribble:scribble];
   
-  // setup default stroke color and size
-  NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-  CGFloat redValue = [userDefaults floatForKey:@"red"];
-  CGFloat greenValue = [userDefaults floatForKey:@"green"];
-  CGFloat blueValue = [userDefaults floatForKey:@"blue"];
-  CGFloat sizeValue = [userDefaults floatForKey:@"size"];
+    // setup default stroke color and size
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    CGFloat redValue = [userDefaults floatForKey:@"red"];
+    CGFloat greenValue = [userDefaults floatForKey:@"green"];
+    CGFloat blueValue = [userDefaults floatForKey:@"blue"];
+    CGFloat sizeValue = [userDefaults floatForKey:@"size"];
   
-  [self setStrokeSize:sizeValue];
-  [self setStrokeColor:[UIColor colorWithRed:redValue
-                                       green:greenValue 
-                                        blue:blueValue 
-                                       alpha:1.0]];
+    [self setStrokeSize:sizeValue];
+    [self setStrokeColor:[UIColor colorWithRed:redValue
+                                         green:greenValue
+                                          blue:blueValue
+                                         alpha:1.0]];
 }
 
-
-/*
- // Override to allow orientations other than the default portrait orientation.
- - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
- // Return YES for supported orientations
- return (interfaceOrientation == UIInterfaceOrientationPortrait);
- }
- */
-
-- (void)didReceiveMemoryWarning 
-{
-  // Releases the view if it doesn't have a superview.
-  [super didReceiveMemoryWarning];
-  
-  // Release any cached data, images, etc that aren't in use.
-}
-
-- (void)viewDidUnload 
-{
+- (void)viewDidUnload {
   [super viewDidUnload];
   // Release any retained subviews of the main view.
   // e.g. self.myOutlet = nil;
 }
 
 
-- (void)dealloc 
-{
-  //[canvasView_ release]; //tyler
-  //[scribble_ release];
-  //[super dealloc];
+- (void)dealloc {
 }
 
 #pragma mark -
 #pragma mark Stroke color and size accessor methods
-
-- (void) setStrokeSize:(CGFloat) aSize
-{
-  // enforce the smallest size
-  // allowed
-  if (aSize < 5.0) 
-  {
-    strokeSize_ = 5.0;
-  }
-  else 
-  {
-    strokeSize_ = aSize;
-  }
+- (void) setStrokeSize:(CGFloat) aSize {
+    // enforce the smallest size allowed
+    if (aSize < 2.0) {
+        strokeSize_ = 2.0;
+    } else {
+        strokeSize_ = aSize;
+    }
 }
 
 
@@ -157,92 +123,82 @@
 #pragma mark -
 #pragma mark Touch Event Handlers
 
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
-{
-  startPoint_ = [[touches anyObject] locationInView:canvasView_];
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    startPoint_ = [[touches anyObject] locationInView:canvasView_];
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
-  CGPoint lastPoint = [[touches anyObject] previousLocationInView:canvasView_];
+    CGPoint lastPoint = [[touches anyObject] previousLocationInView:canvasView_];
   
-  // add a new stroke to scribble
-  // if this is indeed a drag from
-  // a finger
-  if (CGPointEqualToPoint(lastPoint, startPoint_))
-  {
-      id <Mark> newStroke = [[Stroke alloc] init];// autorelease];//tyler
-    [newStroke setColor:strokeColor_];
-    [newStroke setSize:strokeSize_];
+    // add a new stroke to scribble if this is indeed a drag from a finger
+    if (CGPointEqualToPoint(lastPoint, startPoint_))
+    {
+        id <Mark> newStroke = [[Stroke alloc] init];// autorelease];//tyler
+        [newStroke setColor:strokeColor_];
+        [newStroke setSize:strokeSize_];
     
-    //[scribble_ addMark:newStroke shouldAddToPreviousMark:NO];
+        //[scribble_ addMark:newStroke shouldAddToPreviousMark:NO];
     
-    // retrieve a new NSInvocation for drawing and
-    // set new arguments for the draw command
-    NSInvocation *drawInvocation = [self drawScribbleInvocation];
-    [drawInvocation setArgument:&newStroke atIndex:2];
+        // retrieve a new NSInvocation for drawing and set new arguments for the draw command
+        NSInvocation *drawInvocation = [self drawScribbleInvocation];
+        [drawInvocation setArgument:&newStroke atIndex:2];
     
-    // retrieve a new NSInvocation for undrawing and
-    // set a new argument for the undraw command
-    NSInvocation *undrawInvocation = [self undrawScribbleInvocation];
-    [undrawInvocation setArgument:&newStroke atIndex:2];
+        // retrieve a new NSInvocation for undrawing and set a new argument for the undraw command
+        NSInvocation *undrawInvocation = [self undrawScribbleInvocation];
+        [undrawInvocation setArgument:&newStroke atIndex:2];
     
-    // execute the draw command with the undraw command
-    [self executeInvocation:drawInvocation withUndoInvocation:undrawInvocation];
-  }
+        // execute the draw command with the undraw command
+        [self executeInvocation:drawInvocation withUndoInvocation:undrawInvocation];
+    }
   
-  // add the current touch as another vertex to the
-  // temp stroke
-  CGPoint thisPoint = [[touches anyObject] locationInView:canvasView_];
-  Vertex *vertex = [[Vertex alloc]
-                     initWithLocation:thisPoint];// autorelease]; //tyler
+    // add the current touch as another vertex to the temp stroke
+    CGPoint thisPoint = [[touches anyObject] locationInView:canvasView_];
+    Vertex *vertex = [[Vertex alloc] initWithLocation:thisPoint];// autorelease]; //tyler
   
-  // we don't need to undo every vertex
-  // so we are keeping this
-  [scribble_ addMark:vertex shouldAddToPreviousMark:YES];
+    // we don't need to undo every vertex so we are keeping this
+    [scribble_ addMark:vertex shouldAddToPreviousMark:YES];
 }
 
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
-{
-  CGPoint lastPoint = [[touches anyObject] previousLocationInView:canvasView_];
-  CGPoint thisPoint = [[touches anyObject] locationInView:canvasView_];
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    CGPoint lastPoint = [[touches anyObject] previousLocationInView:canvasView_];
+    CGPoint thisPoint = [[touches anyObject] locationInView:canvasView_];
   
-  // if the touch never moves (stays at the same spot until lifted now)
-  // just add a dot to an existing stroke composite
-  // otherwise add it to the temp stroke as the last vertex
-  if (CGPointEqualToPoint(lastPoint, thisPoint))
-  {
-    Dot *singleDot = [[Dot alloc]
-                       initWithLocation:thisPoint];//autorelease]; //tyler
-    [singleDot setColor:strokeColor_];
-    [singleDot setSize:strokeSize_];
+    // if the touch never moves (stays at the same spot until lifted now)
+    // just add a dot to an existing stroke composite
+    // otherwise add it to the temp stroke as the last vertex
+    if (CGPointEqualToPoint(lastPoint, thisPoint))
+    {
+        Dot *singleDot = [[Dot alloc]
+                          initWithLocation:thisPoint];//autorelease]; //tyler
+        [singleDot setColor:strokeColor_];
+        [singleDot setSize:strokeSize_];
     
-    //[scribble_ addMark:singleDot shouldAddToPreviousMark:NO];
+        //[scribble_ addMark:singleDot shouldAddToPreviousMark:NO];
     
-    // retrieve a new NSInvocation for drawing and
-    // set new arguments for the draw command
-    NSInvocation *drawInvocation = [self drawScribbleInvocation];
-    [drawInvocation setArgument:&singleDot atIndex:2];
+        // retrieve a new NSInvocation for drawing and
+        // set new arguments for the draw command
+        NSInvocation *drawInvocation = [self drawScribbleInvocation];
+        [drawInvocation setArgument:&singleDot atIndex:2];
     
-    // retrieve a new NSInvocation for undrawing and
-    // set a new argument for the undraw command
-    NSInvocation *undrawInvocation = [self undrawScribbleInvocation];
-    [undrawInvocation setArgument:&singleDot atIndex:2];
+        // retrieve a new NSInvocation for undrawing and set a new argument for the undraw command
+        NSInvocation *undrawInvocation = [self undrawScribbleInvocation];
+        [undrawInvocation setArgument:&singleDot atIndex:2];
     
-    // execute the draw command with the undraw command
-    [self executeInvocation:drawInvocation withUndoInvocation:undrawInvocation];
-  }
+        // execute the draw command with the undraw command
+        [self executeInvocation:drawInvocation withUndoInvocation:undrawInvocation];
+    }
    
-  // reset the start point here
-  startPoint_ = CGPointZero;
+    // reset the start point here
+    startPoint_ = CGPointZero;
   
-  // if this is the last point of stroke
-  // don't bother to draw it as the user
-  // won't tell the difference
+    // if this is the last point of stroke don't bother to draw it as the user won't tell the difference
 }
 
-- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
-{
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    
   // reset the start point here
   startPoint_ = CGPointZero;
 }
@@ -250,7 +206,6 @@
 
 #pragma mark -
 #pragma mark Scribble observer method
-
 - (void) observeValueForKeyPath:(NSString *)keyPath 
                        ofObject:(id)object 
                          change:(NSDictionary *)change 
@@ -269,59 +224,69 @@
 #pragma mark -
 #pragma mark Draw Scribble Invocation Generation Methods
 
-- (NSInvocation *) drawScribbleInvocation
-{
-  NSMethodSignature *executeMethodSignature = [scribble_ 
+- (NSInvocation *) drawScribbleInvocation {
+    
+    NSMethodSignature *executeMethodSignature = [scribble_
                                                methodSignatureForSelector:
                                                @selector(addMark:
                                                          shouldAddToPreviousMark:)];
-  NSInvocation *drawInvocation = [NSInvocation 
+    NSInvocation *drawInvocation = [NSInvocation
                                   invocationWithMethodSignature:
                                   executeMethodSignature];
-  [drawInvocation setTarget:scribble_];
-  [drawInvocation setSelector:@selector(addMark:shouldAddToPreviousMark:)];
-  BOOL attachToPreviousMark = NO;
-  [drawInvocation setArgument:&attachToPreviousMark atIndex:3];
+    [drawInvocation setTarget:scribble_];
+    [drawInvocation setSelector:@selector(addMark:shouldAddToPreviousMark:)];
+    BOOL attachToPreviousMark = NO;
+    [drawInvocation setArgument:&attachToPreviousMark atIndex:3];
   
-  return drawInvocation;
+    return drawInvocation;
 }
 
-- (NSInvocation *) undrawScribbleInvocation
-{
-  NSMethodSignature *unexecuteMethodSignature = [scribble_ 
+- (NSInvocation *) undrawScribbleInvocation {
+    
+    NSMethodSignature *unexecuteMethodSignature = [scribble_
                                                  methodSignatureForSelector:
                                                  @selector(removeMark:)];
-  NSInvocation *undrawInvocation = [NSInvocation 
+    NSInvocation *undrawInvocation = [NSInvocation
                                     invocationWithMethodSignature:
                                     unexecuteMethodSignature];
-  [undrawInvocation setTarget:scribble_]; 
-  [undrawInvocation setSelector:@selector(removeMark:)];
-  
-  return undrawInvocation;
+    [undrawInvocation setTarget:scribble_];
+    [undrawInvocation setSelector:@selector(removeMark:)];
+    
+    return undrawInvocation;
 }
 
 #pragma mark Draw Scribble Command Methods
-
+//命令模式-实现: 撤销操作、恢复操作;
 - (void) executeInvocation:(NSInvocation *)invocation 
-        withUndoInvocation:(NSInvocation *)undoInvocation
-{
-  [invocation retainArguments];
+        withUndoInvocation:(NSInvocation *)undoInvocation {
+    
+    //保留参数 和 返回值
+    [invocation retainArguments];
 
-  [[self.undoManager prepareWithInvocationTarget:self] 
-   unexecuteInvocation:undoInvocation
-   withRedoInvocation:invocation];
+    [[self.undoManager prepareWithInvocationTarget:self]
+     unexecuteInvocation:undoInvocation withRedoInvocation:invocation];
   
-  [invocation invoke];
+    [invocation invoke];
 }
 
 - (void) unexecuteInvocation:(NSInvocation *)invocation 
           withRedoInvocation:(NSInvocation *)redoInvocation
 {  
   [[self.undoManager prepareWithInvocationTarget:self] 
-   executeInvocation:redoInvocation
-   withUndoInvocation:invocation];
+   executeInvocation:redoInvocation withUndoInvocation:invocation];
   
   [invocation invoke];
 }
 
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+/*
+ // Override to allow orientations other than the default portrait orientation.
+ - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+ // Return YES for supported orientations
+ return (interfaceOrientation == UIInterfaceOrientationPortrait);
+ }
+ */
 @end
